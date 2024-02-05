e33a53379118b07cf28a1d82f696142a81c02fc1
Zeynep Erdogru <46981050+zeytnep@users.noreply.github.com>
2024-02-05 13:34:50 -0500

    updated manifest and the background files

---
+++ background.js
@@ -0,0 +1,14 @@
+// background.js
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // Query the active tab in the current window
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    // Extract information about the first tab (usually the only one)
+    var currentTab = tabs[0];
+
+    // Extract the URL of the current tab
+    var url = currentTab.url;
+
+    // Display the URL in the console
+    alert("Current URL of the website: xoxox " + url);
+  });
+});
